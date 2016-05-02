30d6149c090363b8f8b5680319fd4990d0739f9a
Seth Trowbridge <seth111@gmail.com>
2016-05-02 14:58:32 -0400

    canvas directive

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+/*
 var drawing = function(inSocket, inJQCanvas) {
     
     var canvas, context;
@@ -47,3 +48,64 @@ $(document).ready(function() {
     drawing(socket, $('#canvas'));
     guessing(socket, $('#guess'));
 });
+*/
+
+
+var Pictionary = angular.module("Pictionary", []);
+Pictionary.factory("FactorySocket", [function(){
+    return io();
+}]);
+Pictionary.directive("canvas", ["FactorySocket", function(inSocket){
+    return {
+        restrict:'E',
+        link: function(inScope, inElement, inAttributes){
+            
+            console.log('linkeing');
+            
+            var canvas, context;
+            var handlerDown, handlerMove, handlerUp;
+            var draw;
+            
+            canvas = inElement;
+            
+            console.log(canvas[0].getBoundingClientRect());
+            
+            context = canvas[0].getContext('2d');
+            canvas[0].width = 800;
+            canvas[0].height = 600;
+            canvas.css({width:"800px", height:"600px"});
+            
+            draw = function(position) {
+                context.beginPath();
+                context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
+                context.fill();
+            };
+            inSocket.on('draw', draw);
+
+            handlerDown = function(inEvent){
+                canvas.unbind('mousedown', handlerDown);
+                angular.element(document).bind('mouseup', handlerUp);
+                canvas.bind('mousemove', handlerMove);
+            };
+            handlerMove = function(inEvent){
+
+                var offset = canvas[0].getBoundingClientRect();
+                var pos = {x:inEvent.pageX - offset.left, y:inEvent.pageY - offset.top};
+
+                console.log(pos)
+
+                draw(pos);
+                inSocket.emit('draw', pos);
+            };
+            handlerUp = function(inEvent){
+                angular.element(document).unbind('mouseup', handlerUp);
+                canvas.unbind('mousemove', handlerMove);
+                canvas.bind('mousedown', handlerDown);
+            };
+            handlerUp(null);
+        }  
+    };
+}]);
+Pictionary.controller("ControllerPictionary", ["$scope", function(inScope){
+    console.log("at scope");
+}]);
